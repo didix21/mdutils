31fb56edacd89393211a5b6c6bb7493bfd309630
didix21 <didaccoll_93@hotmail.com>
2018-04-07 19:09:07 +0200

    Created NewFile class.

---
+++ mdutils/fileutils/fileutils.py
@@ -0,0 +1,34 @@
+# Python
+#
+# This module implements a main class that allows to create markdown files, write in them or read.
+#
+# This file is part of mdutils. https://github.com/didix21/mdutils
+#
+# (C) 2018 Dídac Coll
+#
+# SPDX-License-Identifier:   BSD-3-Clause
+
+class NewFile(object):
+    """NewFile class creates a new file of MarkDown extension.
+
+    Features available are:
+    - Create a file.
+    - Rewrite all the file with new data.
+    - Write at the end of the file."""
+    def __init__(self, name):
+        self.file_name = name + '.md'
+        self.file = open(self.file_name, 'w+', encoding='UTF-8')
+        self.file.close()
+
+    def rewrite_all_file(self, data):
+        with open(self.file_name, 'w', encoding='utf-8') as self.file:
+            self.file.write(data)
+
+    def append_end(self, data):
+        with open(self.file_name, 'a') as self.file:
+            self.file.write(data)
+
+
+if __name__ == '__main__':
+    new_file = NewFile('Example')
+    new_file.add(data="# Some Text Example")
