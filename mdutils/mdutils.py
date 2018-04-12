bf1ef0d900afbb72b8a63ca92163f49427bb70f4
Dídac Coll Pujals <didaccoll_93@hotmail.com>
2018-04-12 19:28:58 +0200

    Now the table of contents will be written at the second line of Markdown file.

--- mdutils/mdutils.py
+++ mdutils/mdutils.py
@@ -1,6 +1,6 @@
 # Python
 #
-# This module implements a main class that allows to create markdown files, write in them or read.
+# This module implements the Main Markdown class.
 #
 # This file is part of mdutils. https://github.com/didix21/mdutils
 #
@@ -21,112 +21,10 @@ The different features availables are:
     * Insert Code.
 """
 from mdutils.fileutils.fileutils import NewFile
+from mdutils.tools.tools import Header
+from mdutils.tools.tools import TableOfContents
 
 
-class Header(object):
-    """Contain the main methods to define Headers on a Markdown file.
-
-    """
-    # ********************************************************************
-    # *                             Atx-Style                            *
-    # ********************************************************************
-    @staticmethod
-    def atx_level_1(title):
-        return '# ' + title
-
-    @staticmethod
-    def atx_level_2(title):
-        return '\n\n## ' + title
-
-    @staticmethod
-    def atx_level_3(title):
-        return '\n\n### ' + title
-
-    @staticmethod
-    def atx_level_4(title):
-        return '\n\n#### ' + title
-
-    @staticmethod
-    def atx_level_5(title):
-        return '\n\n##### ' + title
-
-    @staticmethod
-    def atx_level_6(title):
-        return '\n\n###### ' + title
-
-    # ********************************************************************
-    # *                          Setext-Style                            *
-    # ********************************************************************
-    @staticmethod
-    def setext_level_1(title):
-        return title + '\n' + ''.join(['=' for _ in title])
-
-    @staticmethod
-    def setext_level_2(title):
-        return title + '\n' + ''.join(['-' for _ in title])
-
-    def choose_header(self, level, title, style='atx'):
-        """ This method choose the style and the header level.
-
-            :Examples:
-            >>> import Header
-            >>> createHeaders = Header()
-            >>> createHeaders.choose_header(level=1, title='New Header', style='atx')
-            "# New Header"
-
-            >>> createHeaders.choose_header(level=1, title='Another Header 1', style=setext)
-            "Another Header 1\\n----------------"
-
-        :param level: Header Level, For Atx-style 1 til 6. For Setext-style 1 and 2 header levels.
-        :param title: Header Title.
-        :param style: Header Style atx or setext.
-        :return:
-        """
-        if style.lower() == 'atx':
-            if level == 1:
-                return self.atx_level_1(title)
-            elif level == 2:
-                return self.atx_level_2(title)
-            elif level == 3:
-                return self.atx_level_3(title)
-            elif level == 4:
-                return self.atx_level_4(title)
-            elif level == 5:
-                return self.atx_level_5(title)
-            elif level == 6:
-                return self.atx_level_6(title)
-        elif style.lower() == 'setext':
-            if level == 1:
-                return self.setext_level_1(title)
-            elif level == 2:
-                return self.setext_level_2(title)
-
-
-class TableOfContents(object):
-    def _loop_trought(self, elements, tab=''):
-        """Method that go trough a list of elements that contain strings and other list and return a string \
-        reade to be written inside a markdown file in order to create a table of contents.
-
-        - **elements**: contain all the headers defined on the main class.
-        - **tab:** Inserts tabulations."""
-        elements_to_string = ""
-        for item in elements:
-            if isinstance(item, list):
-                if item != []:
-                    if tab == '\t':
-                        elements_to_string += self._loop_trought(item, tab='\t\t')
-                    else:
-                        elements_to_string += self._loop_trought(item, tab='\t')
-            else:
-                elements_to_string += '\n' + tab + '* [' + item + '](#' + item.lower().replace(' ', '-') + ')'
-
-        return elements_to_string
-
-    def create_table_of_contents(self, array_of_title_contents):
-        table_of_contents = ""
-        table_of_contents += self._loop_trought(array_of_title_contents)
-
-        return table_of_contents
 
 
 class MdUtils:
@@ -146,7 +44,7 @@ class MdUtils:
     def create_md_file(self):
         """ It creates a new Markdown file"""
         md_file = NewFile(self.file_name)
-        md_file.rewrite_all_file(data=self.header.choose_header(level=1, title=self.title))
+        md_file.rewrite_all_file(data=self.header.choose_header(level=1, title=self.title, style='setext'))
         return md_file
 
     def new_header(self, level, title, style='atx'):
@@ -168,25 +66,25 @@ class MdUtils:
         self.markdown_file.append_end(self.header.choose_header(level, title, style))
 
     def _add_new_item_table_of_content(self, level, item):
-        if level == 2:
+        if level == 1:
             self._table_titles.append(item)
             self._table_titles.append([])
-        elif level == 3:
+        elif level == 2:
             self._table_titles[-1].append(item)
             self._table_titles[-1].append([])
-        elif level == 4:
-            self._table_titles[-1][-1].append(item)
-            self._table_titles[-1][-1].append([])
-        elif level == 5:
-            self._table_titles[-1][-1][-1].append(item)
-            self._table_titles[-1][-1][-1].append([])
-        elif level == 6:
-            self._table_titles[-1][-1][-1][-1].append(item)
+        # elif level == 3:
+        #     self._table_titles[-1][-1].append(item)
+        #     self._table_titles[-1][-1].append([])
+        # elif level == 4:
+        #     self._table_titles[-1][-1][-1].append(item)
+        #     self._table_titles[-1][-1][-1].append([])
+        # elif level == 5:
+        #     self._table_titles[-1][-1][-1][-1].append(item)
 
     def new_table_of_contents(self):
         """ Add a table of contents of the Header Levels 2, 3 and 4."""
         table = TableOfContents().create_table_of_contents(self._table_titles)
-        self.markdown_file.append_after_first_line(table)
+        self.markdown_file.append_after_second_line(table)
 
 
 if __name__ == '__main__':
