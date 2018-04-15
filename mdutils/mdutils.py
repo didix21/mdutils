41fd444ea8e58593a6765cefe8b8f173da4ebdd1
Dídac Coll Pujals <didaccoll_93@hotmail.com>
2018-04-15 18:03:57 +0200

    Changed some features, now all data will be saved previously in a string and then will be written to Markdown file. Now, create_md_file() is called at the end of the code

--- mdutils/mdutils.py
+++ mdutils/mdutils.py
@@ -10,8 +10,8 @@
 
 """Module **mdutils**
 
-The different features availables are:
-    * Create Heades, Til 6 sub-lvels.
+The available features are:
+    * Create Headers, Til 6 sub-levels.
     * Auto generate a table of contents.
     * Create List and sub-list.
     * Create paragraph.
@@ -21,9 +21,7 @@ The different features availables are:
     * Insert Code.
 """
 from mdutils.fileutils.fileutils import NewFile
-from mdutils.tools.tools import Header
-from mdutils.tools.tools import TableOfContents
-from mdutils.tools.tools import Table
+from mdutils.tools import tools
 
 
 class MdUtils:
@@ -32,18 +30,23 @@ class MdUtils:
     Long description will be written.
 
     """
+
     def __init__(self, file_name, title="", author=""):
         self.file_name = file_name
-        self.title = title
+
         self.author = author
-        self.header = Header()
-        self.markdown_file = self.create_md_file()
+        self.header = tools.Header()
+        self.textUtils = tools.TextUtils()
+        self.title = self.header.choose_header(level=1, title=title, style='setext')
+        self.table_of_contents = ""
+        self.data_text = ""
         self._table_titles = []
 
     def create_md_file(self):
         """ It creates a new Markdown file"""
         md_file = NewFile(self.file_name)
-        md_file.rewrite_all_file(data=self.header.choose_header(level=1, title=self.title, style='setext'))
+        md_file.rewrite_all_file(data=self.title + self.table_of_contents + self.data_text)
+
         return md_file
 
     def new_header(self, level, title, style='atx'):
@@ -52,7 +55,7 @@ class MdUtils:
             ``new_header(level=2, title='Header Title', style='atx')``
             This will write a new level 2 Atx-style header on file_name.md:
                 * ## Header Title
-            ``new_header(level=2, title='Header Title', style='setext)``
+            ``new_header(level=2, title='Header Title', style='setext')``
             This will write a new level 2 Setext-style header on file_name.md:
                 * Header Title
                 '-------------'
@@ -62,7 +65,7 @@ class MdUtils:
         :param style: Header style, could be ``'atx'`` or ``'setext'``. By default **atx* style is chosen.
         """
         self._add_new_item_table_of_content(level, title)
-        self.markdown_file.append_end(self.header.choose_header(level, title, style))
+        self.data_text += self.header.choose_header(level, title, style)
 
     def _add_new_item_table_of_content(self, level, item):
         if level == 1:
@@ -71,25 +74,25 @@ class MdUtils:
         elif level == 2:
             self._table_titles[-1].append(item)
             self._table_titles[-1].append([])
-        # elif level == 3:
-        #     self._table_titles[-1][-1].append(item)
-        #     self._table_titles[-1][-1].append([])
-        # elif level == 4:
-        #     self._table_titles[-1][-1][-1].append(item)
-        #     self._table_titles[-1][-1][-1].append([])
-        # elif level == 5:
-        #     self._table_titles[-1][-1][-1][-1].append(item)
 
-    def new_table_of_contents(self):
+    def new_table_of_contents(self, table_title="Table of contents"):
         """ Add a table of contents of the Header Levels 2, 3 and 4."""
-        table = TableOfContents().create_table_of_contents(self._table_titles)
-        self.markdown_file.append_after_second_line(table)
+        self.table_of_contents += self.header.choose_header(level=1, title=table_title, style='setext')
+        self.table_of_contents += tools.TableOfContents().create_table_of_contents(self._table_titles)
+
+        return self.table_of_contents
 
     def create_table(self, columns, rows, text, text_align='center'):
-        new_table = Table()
+        new_table = tools.Table()
         text_table = new_table.create_table(columns, rows, text, text_align)
-        self.markdown_file.append_end(text_table)
+        self.data_text += text_table
+
+        return text_table
+
+    def add_new_paragraph(self, text=''):
+        self.data_text += '\n' + text + '\n'
+        return self.data_text
 
 
 if __name__ == '__main__':
-    heeader = Header()
+    header = tools.Header()
---
+++ test.py
@@ -0,0 +1,37 @@
+from mdutils.mdutils import MdUtils
+
+md_file = MdUtils(file_name='Test', title='Test Title')
+
+text_array = ['**Test**', '**Descripción**', '**Estado**',
+              'Test 1', 'Carga de configuración correcta', md_file.textUtils.text_color("OK", 'green'),
+              'Test 2', 'Lectura de Configuración', md_file.textUtils.text_color("NOK", 'red'),
+              'Test 3', 'Lectura de Soporte', md_file.textUtils.text_color("OK", 'green'),
+              'Test 4', 'Modificación de entradas y lectura de salidas de cantón', md_file.textUtils.text_color("OK", 'green'),
+              'Test 5', 'Lectura de estados de Pedal de Rearme y Aviso', md_file.textUtils.text_color("OK", 'green'),
+              'Test 6', 'Actualización de datos de unidades de vía', md_file.textUtils.text_color("OK", 'green'),
+              'Test 7', 'Fallos en carga de configuración - Campo IdApp Erróneo', md_file.textUtils.text_color("OK", 'green'),
+              'Test 8', 'Fallos en carga de configuración - Campo VersTAbla Erróneo', md_file.textUtils.text_color("NOK", 'red'),
+              'Test 9', 'Fallos en carga de configuración - Campo IdUc Erróneo', md_file.textUtils.text_color("NOK", 'red'),
+              'Test 10', 'Fallos en carga de configuración - Campo Addresses Erróneo', md_file.textUtils.text_color("NOK", 'red'),
+              'Test 11', 'Fallos en carga de configuración - Campo NumTc Erróneo', md_file.textUtils.text_color("NOK", 'red'),
+              'Test 12', 'Fallos en carga de configuración - Campo NumUv Erróneo', md_file.textUtils.text_color("NOK", 'red'),
+              'Test 13', 'Fallos en carga de configuración - Campo CRC Erróneo', md_file.textUtils.text_color("NOK", 'red'),
+              'Test 14', 'Fallos en carga de configuración - Campo IdTc Erróneo', md_file.textUtils.text_color("NOK", 'red')]
+
+
+md_file.new_header(1, "Results Tests")
+md_file.create_table(3, 15, text_array)
+md_file.new_header(1, "Test Details")
+md_file.add_new_paragraph("All test will be written at the following lines.")
+md_file.new_header(2, "Test 1")
+md_file.new_header(2, "Test 2")
+md_file.new_header(2, "Test 3")
+md_file.new_header(3, "Test 3.0")
+md_file.new_header(4, "Test 3.0.0")
+md_file.new_header(4, "Test 3.0.1")
+md_file.new_header(3, "Test 3.1")
+md_file.new_header(2, "Test 4")
+
+md_file.new_table_of_contents()
+
+md_file.create_md_file()
