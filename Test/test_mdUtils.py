a7ba391b5fb31daa472cf8ae0805ace3d6766803
Dídac Coll Pujals <didaccoll_93@hotmail.com>
2018-04-15 22:03:26 +0200

    Added test_mdUtils.py file.

---
+++ Test/test_mdUtils.py
@@ -0,0 +1,18 @@
+from unittest import TestCase
+
+
+class TestMdUtils(TestCase):
+    def test_create_md_file(self):
+        self.fail()
+
+    def test_new_header(self):
+        self.fail()
+
+    def test__add_new_item_table_of_content(self):
+        self.fail()
+
+    def test_new_table_of_contents(self):
+        self.fail()
+
+    def test_create_table(self):
+        self.fail()
