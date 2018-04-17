675e2154df34e8a00f497e88aa64258357847282
Dídac Coll Pujals <didaccoll_93@hotmail.com>
2018-04-17 21:55:11 +0200

    test_create_md_file and test_new_header have been implemented

--- Test/test_mdUtils.py
+++ Test/test_mdUtils.py
@@ -1,12 +1,34 @@
 from unittest import TestCase
+from mdutils.mdutils import MdUtils
 
+from pathlib import Path
+import os
 
 class TestMdUtils(TestCase):
     def test_create_md_file(self):
-        self.fail()
+        md_file = MdUtils("Test_file")
+        md_file.create_md_file()
+        md_file_expect = Path('Test_file.md')
+        if md_file_expect.is_file():
+            os.remove('Test_file.md')
+            pass
+        else:
+            self.fail()
 
     def test_new_header(self):
-        self.fail()
+        file_name = 'Test_file'
+        md_file = MdUtils(file_name)
+        string_headers_expected = "\n# Header 0\n\n## Header 1\n\n### Header 2\n\n#### Header 3\n\n" \
+                                  "##### Header 4\n\n###### Header 5\n"
+        string_headers = ""
+        for x in range(6):
+            string_headers += md_file.new_header(level=(x + 1), title='Header ' + str(x), style='atx')
+
+        self.assertEqual(string_headers, string_headers_expected)
+        md_file.create_md_file()
+        file_result = md_file.read_md_file(file_name)
+        self.assertEqual(file_result, '\n\n\n' + string_headers_expected)
+        os.remove(file_name + '.md')
 
     def test__add_new_item_table_of_content(self):
         self.fail()
