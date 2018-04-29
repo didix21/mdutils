4dbd7ae70f26ef4a186a57bb2a7c7dd642005e52
Dídac Coll Pujals <didaccoll_93@hotmail.com>
2018-04-29 13:56:10 +0200

    New beta version. Deleted tildes on maintainer field.

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
 setup(name='mdutils',
-      version=1.0,
+      version='1.0.0b2',
       license='MIT',
       author='Didac Coll',
       author_email='didaccoll_93@hotmail.com',
-      maintainer='Dídac Coll',
+      maintainer='Didac Coll',
       maintainer_email='didaccoll_93@hotmail.com',
       description='A package, useful to create Markdown files while executing python code.',
       long_description=open('./doc/source/README.rst').read(),
