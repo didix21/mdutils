9a6e3644ebbf9ec3a6a17cb6ce7b9d68c4c15723
Dídac Coll Pujals <dcoll.ext@electrans.es>
2018-10-13 15:03:45 +0200

    Improved test link

--- Test/test_tools/test_link.py
+++ Test/test_tools/test_link.py
@@ -14,8 +14,20 @@ class TestLink(TestCase):
     def test_new_link(self):
         a_link = "https://github.com/didix21/mdutils"
         title = "Github Didac"
-        expected = '[' + title + '](' + a_link + ')'
+        reference = "Reference Style"
+        global_references = {}
 
-        actual = Link(title, a_link, style="inline")
+        expected_inline_style = '[' + title + '](' + a_link + ')'
+        actual_inline_style = Link(title, a_link, "inline", global_references)
+        self.assertEqual(expected_inline_style, actual_inline_style.new_link())
 
-        self.assertEqual(expected, actual.new_link())
+        expected_reference_style = '[' + title +'][' + reference + ']'
+        actual_reference_style = Link(title, a_link, "reference-Reference Style", global_references)
+        self.assertEqual(expected_reference_style, actual_reference_style.new_link())
+
+        global_references = actual_reference_style.get_global_references()
+        self.assertEqual({reference: a_link}, global_references)
+
+
+
+
