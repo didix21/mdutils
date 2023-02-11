b507e698ed62193cb62ca95486f38e845992c9a4
Dídac Coll <didac.coll@qustodio.com>
2023-02-11 01:29:10 +0100

    Add script for updating version on different files

---
+++ scripts/update-release.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+
+import getopt, sys
+import re
+
+def usage():
+    print(
+        """
+        You can use the scripts as follows
+        ./update-release.py -v v1.5.0
+        """
+    )
+
+def replace_on(file, pattern, new_value):
+    with open(file, 'r+') as file:
+        content = file.read()
+        new_content = re.sub(pattern, new_value, content)
+        file.write(new_content)
+
+def replace_version_on_config_py(version):
+    file_name = 'doc/source/conf.py'
+    replace_on(file_name, r"release = '(\d+).(\d+).(\d+)'", f"release = '{version}'")
+
+def replace_version_on_setup_py(version):
+    file_name = 'setup.py'
+    replace_on(file_name, r"version='(\d+).(\d+).(\d+)'", f"version='{version}'")
+
+def replace_version_on_changelog_gen(version):
+    file_name = '.github_changelog_generator'
+    replace_on(file_name, r"future-release=v(\d+).(\d+).(\d+)", f"future-release={version}")
+
+
+def main():
+    version = None
+    try:
+        options, args = getopt.getopt(sys.argv[1:], "v:", ["version="])
+    except getopt.GetoptError as err:
+            # print help information and exit:
+            print(err)  # will print something like "option -a not recognized"
+            usage()
+            sys.exit(2)
+
+    for option, value in options:
+        if option in ("-v", "--version"):
+            version = value
+        else:
+            assert False, "Unhandled option"
+
+    version_number = version[1:] if 'v' in version else version
+    replace_version_on_setup_py(version_number)
+    replace_version_on_config_py(version_number)
+    replace_version_on_changelog_gen(version)
+
+if __name__ == "__main__":
+    main()
